fccc2e04cac1f4314a3ef67ba9d7137907db84f2
sergiidmytryshyn <dmytryshyn.pn@ucu.edu.ua>
2025-03-29 00:31:24 +0200

    Lab4 done

--- facade_service.py
+++ facade_service.py
@@ -6,11 +6,15 @@ import logging_service_pb2
 import logging_service_pb2_grpc
 from fastapi import FastAPI
 from tenacity import retry, stop_after_attempt, wait_exponential
+from hazelcast import HazelcastClient
 
 app = FastAPI()
 
 LOGGING_SERVICE_ADDRESSES = ["localhost:8081", "localhost:8082", "localhost:8083"]
-MESSAGES_SERVICE_URL = "http://localhost:8001"
+MESSAGE_SERVICE_ADDRESSES = ["http://localhost:8001", "http://localhost:8002"]
+
+hz_client = HazelcastClient(cluster_name="hello-world")
+msg_queue = hz_client.get_queue("messages-queue").blocking()
 
 @retry(stop=stop_after_attempt(5), wait=wait_exponential(multiplier=1))
 async def send_log_message(msg):
@@ -30,6 +34,8 @@ async def send_log_message(msg):
 @app.post("/send")
 async def send_message(msg: str):
     await send_log_message(msg)
+    msg_queue.offer(msg)
+    return {"status": "message sent"}
 
 @app.get("/get")
 async def get_messages():
@@ -40,9 +46,16 @@ async def get_messages():
             with grpc.insecure_channel(LOGGING_SERVICE_ADDRESS) as channel:
                 stub = logging_service_pb2_grpc.LoggingServiceStub(channel)
                 log_response = stub.GetLogs(logging_service_pb2.Empty())
+            
+            msg_service_url = random.choice(MESSAGE_SERVICE_ADDRESSES)
             async with httpx.AsyncClient() as client:
-                msg_response = await client.get(f"{MESSAGES_SERVICE_URL}/message")    
-            return log_response.messages, msg_response.text
+                msg_response = await client.get(f"{msg_service_url}/message")
+
+            return {
+                "logs": log_response.messages,
+                "messages": msg_response.json()
+            }
+
         except grpc.RpcError:
             addresses.remove(LOGGING_SERVICE_ADDRESS)
     return {"error": "No available logging services"}
--- messages_service.py
+++ messages_service.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
+from hazelcast import HazelcastClient
+import threading
 
 app = FastAPI()
+messages = []
+
+hz_client = HazelcastClient(cluster_name="hello-world")
+msg_queue = hz_client.get_queue("messages-queue").blocking()
+
+def consume_messages():
+    while True:
+        msg = msg_queue.take()
+        print(f"[Messages-Service] Got message: {msg}")
+        messages.append(msg)
+
+t = threading.Thread(target=consume_messages, daemon=True)
+t.start()
 
 @app.get("/message")
 async def get_message():
-    return "not implemented yet"
+    return {"messages": messages}
